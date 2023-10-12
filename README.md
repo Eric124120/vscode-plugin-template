4d26c59f58567f652511045ea3ff2c6826bd3b76
黄承文 <chengwen@ssc-hn.com>
2023-10-12 18:04:08 +0800

    main: init

---
+++ README.md
@@ -0,0 +1,17 @@
+# vscode-plugin-template
+
+vscode 插件模板
+
+## 开发环境
+### 1、开环境启动
+```bash
+## 安装依赖
+yarn
+## 编译页面代码
+yarn dev
+```
+### 启动插件
+按“F5”或者“运行->启动调试”，就可以运行起来插件了，第一次运行会进行编译，然后会拉起一个新的 VSCode 窗口
+### 启动插件命令
+在新拉起的VSCode 窗口，按“Ctrl+Shift+P”或者“Command+Shift+P”，弹出如下命令窗，输入“Hello World”
+
---
+++ src/view/App.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsx_runtime_1 = require("react/jsx-runtime");
+function App() {
+    return ((0, jsx_runtime_1.jsx)("h2", { children: "Webview-react" }));
+}
+exports.default = App;
+//# sourceMappingURL=App.js.map
